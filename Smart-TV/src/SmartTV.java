9bde97c3e5db97e2cfcac5fc4c1c3efa1ca5c62b
Gustavo Horst <gustavohorst08@gmail.com>
2024-05-02 16:52:07 -0300

    create class

---
+++ Smart-TV/src/SmartTV.java
@@ -0,0 +1,6 @@
+public class SmartTV {
+    boolean ligad=false;
+    int canal=1; 
+    int volume=1;
+    
+}
--- Smart-TV/src/usuario.java
+++ Smart-TV/src/usuario.java
@@ -1,4 +1,4 @@
-public class App {
+public class usuario {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
